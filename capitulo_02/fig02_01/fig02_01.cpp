9b578221dc5ffbd777e287b049b84c6942f45a8f
Anderson Misson <missondeveloper@gmail.com>
2020-04-18 02:10:08 -0300

    fig02_01 - Impressão de Texto

---
+++ capitulo_02/fig02_01/fig02_01.cpp
@@ -0,0 +1,13 @@
+// Autor: Anderson Misson
+// Figura 2.1: fig02_01.cpp
+// Programa de impressão de texto.
+#include<iostream> // permite que o programa gere saída de dados na tela
+
+// a função main inicia a execução do programa
+int main()
+{
+	std::cout <<"Hello World!\n"; // exibe mensagem
+
+	return 0;
+
+} // fim da função main
