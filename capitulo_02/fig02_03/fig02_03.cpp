330c48d7c0a0d11111993d99e80281b2b6408783
Anderson Misson <missondeveloper@gmail.com>
2020-04-28 02:39:43 -0300

    fig02_03 - Imprimindo texto com múltiplas instruções

---
+++ capitulo_02/fig02_03/fig02_03.cpp
@@ -0,0 +1,15 @@
+// Figura 2.3: fig02_03.cpp
+// Imprimindo uma linha de texto com múltiplas instruções.
+// Autor: A. Misson
+#include<iostream> // permite que o programa gere saída de dados na tela
+
+// a função main inicia a execução do programa
+int main(){
+
+std::cout << "Bem Vindo ";
+std::cout << "ao C++!\n";
+
+return 0; // indica que o programa terminou com sucesso
+
+} // fim da função main
+
