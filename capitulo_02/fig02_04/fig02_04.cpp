f5e771708c724ff499577a5a80e4310f0921a39e
Anderson Misson <missondeveloper@gmail.com>
2020-04-28 22:58:09 -0300

    fig02_04 - Imprimindo múltiplas linhas de texto com única instrução

---
+++ capitulo_02/fig02_04/fig02_04.cpp
@@ -0,0 +1,12 @@
+// Figura 2.4: fig02_04.cpp
+// Imprimindo múltiplas linhas de texto com uma única instrução
+
+#include <iostream> // permite que o programa gere saída de dados na tela
+
+// a função main inicia a execução do programa
+int main(){
+	std::cout << "Bem Vindo\nao\n\nC++!\n";
+
+	return 0; //indica que o programa terminou com sucesso
+
+} // fim da função main
