1e27c26614f9e724debf876395a6486e5fe8e824
Anderson Misson <missondeveloper@gmail.com>
2020-04-30 19:55:06 -0300

    ex02_17 - 1 2 3 4 na mesma linha

---
+++ capitulo_02/ex02_17/ex02_17.cpp
@@ -0,0 +1,19 @@
+// Exercício 02_17 : ex02_17.cpp
+// Escrever 1 2 3 4 de formas diferentes
+// Autor: Anderson Misson
+
+#include<iostream>
+
+	using std::cout;
+
+int main(){
+
+	cout << "1 2 3 4";
+	cout << 1 << 2 << 3 << 4;
+	cout << "1";
+	cout << "2";
+	cout << "3";
+	cout << "4";
+
+	return 0;
+}
