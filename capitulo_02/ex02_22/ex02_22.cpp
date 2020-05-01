b87040dcae4a2c0248aadc6171a4c1114491308a
Anderson Misson <missondeveloper@gmail.com>
2020-05-01 00:09:59 -0300

    ex02_22 - O que o código imprime?

---
+++ capitulo_02/ex02_22/ex02_22.cpp
@@ -0,0 +1,15 @@
+// Exercício 02_22: ex02_22.cpp
+// O que o código imprime?
+// Autor: Anderson Misson
+
+#include<iostream>
+
+	using std::cout;
+	using std::endl;
+
+int main(){
+
+	cout << "*\n**\n***\n****\n*****" << endl;
+
+	return 0;
+}
