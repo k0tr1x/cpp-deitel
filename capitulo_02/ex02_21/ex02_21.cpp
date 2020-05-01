b1488d7c50208efeb28c9c6af5295bbdcb047af1
Anderson Misson <missondeveloper@gmail.com>
2020-04-30 23:57:14 -0300

    ex02_21 - Desenhando com *

---
+++ capitulo_02/ex02_21/ex02_21.cpp
@@ -0,0 +1,64 @@
+// Exercício 02_21: ex02_21.cpp
+// Desenhando com *
+// Autor: Anderson Misson
+
+#include<iostream>
+
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
+int main(){
+
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+
+	cout << "\n";
+	
+	cout << "   ***   \n"
+	     << " *     * \n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << " *     * \n"
+	     << "   ***   \n";
+
+	cout << "\n";
+
+	cout << "    *    \n"
+	     << "   ***   \n"
+	     << "  *****  \n"
+	     << "    *    \n"
+	     << "    *    \n"
+	     << "    *    \n"
+	     << "    *    \n"
+	     << "    *    \n"
+	     << "    *    \n";
+
+	cout << "\n";
+
+	cout << "    *    \n"
+	     << "   * *   \n"
+	     << "  *   *  \n"
+	     << " *     * \n"
+	     << "*       *\n"
+	     << " *     * \n"
+	     << "  *   *  \n"
+	     << "   * *   \n"
+	     << "    *    \n";
+
+	cout << "\n";
+
+	return 0;
+}
+
+
