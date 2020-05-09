b77b0eccd69cf1e2f3095ac6d269420a6a780e46
Anderson Misson <missondeveloper@gmail.com>
2020-05-09 19:18:15 -0300

    ex02_26 - Desenhando com *

---
+++ capitulo_02/ex02_26/ex02_26.cpp
@@ -0,0 +1,23 @@
+// Exercício 02_26: ex02_26.cpp
+// Fazer desenho com Laço
+// Autor: Anderson Misson
+
+#include<iostream>
+
+	using std::cout;
+	using std::endl;
+
+int main(){
+
+	cout << "* * * * * * * * \n";
+	cout << " * * * * * * * *\n";
+	cout << "* * * * * * * * \n";
+	cout << " * * * * * * * *\n";
+	cout << "* * * * * * * * \n";
+	cout << " * * * * * * * *\n";
+	cout << "* * * * * * * * \n";
+	cout << " * * * * * * * *\n";
+	cout << endl;
+
+	return 0;
+}
