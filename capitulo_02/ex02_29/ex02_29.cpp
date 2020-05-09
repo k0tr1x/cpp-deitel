bde2cd84e94816ce6ffaa723c6c9a7603010318e
Anderson Misson <missondeveloper@gmail.com>
2020-05-09 20:21:11 -0300

    ex02_29 - Tabuada Simples

---
+++ capitulo_02/ex02_29/ex02_29.cpp
@@ -0,0 +1,28 @@
+// Exercício 02_29: ex02_29.cpp
+// Tabuada Simples
+// Autor: Anderson Misson
+
+#include<iostream>
+
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
+int main(){
+
+	int n0 = 0, n1 = 1, n2 = 2, n3 = 3, n4 = 4, n5 = 5, n6 = 6, n7 = 7, n8 = 8, n9 = 9, n10 = 10;
+	cout << "INTEIRO" << "\t\t" << "QUADRADO" << "\t" << "CUBO" << endl;
+	cout << n0 << "\t\t" << n0 * n0 << "\t\t" << n0 * n0 * n0 << endl;
+	cout << n1 << "\t\t" << n1 * n1 << "\t\t" << n1 * n1 * n1 << endl;
+	cout << n2 << "\t\t" << n2 * n2 << "\t\t" << n2 * n2 * n2 << endl;
+	cout << n3 << "\t\t" << n3 * n3 << "\t\t" << n3 * n3 * n3 << endl;
+	cout << n4 << "\t\t" << n4 * n4 << "\t\t" << n4 * n4 * n4 << endl;
+	cout << n5 << "\t\t" << n5 * n5 << "\t\t" << n5 * n5 * n5 << endl;
+	cout << n6 << "\t\t" << n6 * n6 << "\t\t" << n6 * n6 * n6 << endl;
+	cout << n7 << "\t\t" << n7 * n7 << "\t\t" << n7 * n7 * n7 << endl;
+	cout << n8 << "\t\t" << n8 * n8 << "\t\t" << n8 * n8 * n8 << endl;
+	cout << n9 << "\t\t" << n9 * n9 << "\t\t" << n9 * n9 * n9 << endl;
+	cout << n10 << "\t\t" << n10 * n10 << "\t\t" << n10 * n10 * n10 << endl;
+
+	return 0;
+}
