7151910dc4dce6033bd4d205ea56f67af19c42cb
Anderson Misson <missondeveloper@gmail.com>
2020-06-09 00:28:43 -0300

    fig04_21 - Incremento e Decremento

---
+++ capitulo_04/fig04_21/fig04_21.cpp
@@ -0,0 +1,24 @@
+// Figura 04_21: fig04_21.cpp
+// Incremento e Decremento
+// Autor Anderson Misson
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   // Pós-Incremento
+   int c = 5;
+   cout << c << endl;
+   cout << c++ << endl;
+   cout << c << endl;
+
+   cout << endl;
+
+   // Pré-Incremento
+   c = 5;
+   cout << c << endl;
+   cout << ++c << endl;
+   cout << c << endl;
+
+}
