19cec7299c62797501592b18237d96ffe68c8d27
Anderson Misson <missondeveloper@gmail.com>
2020-06-13 22:53:03 -0300

    ex04_29 - Loop Infinito com while

---
+++ capitulo_04/ex04_29/ex04_29.cpp
@@ -0,0 +1,17 @@
+// Exercício 04_29: ex04_29.cpp
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int numero = 2;
+
+while(true)
+{
+	cout << numero << endl;
+	numero *= 2;
+}
+
+return 0;
+} // fim do main
