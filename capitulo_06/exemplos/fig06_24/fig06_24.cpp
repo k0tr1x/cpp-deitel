74a2259f7040073cfbe8c577926b1858e552df68
Anderson Misson <missonsama@gmail.com>
2021-12-10 21:59:43 -0300

    fig06_24 - Desfiguração de nomes

---
+++ capitulo_06/exemplos/fig06_24/fig06_24.cpp
@@ -0,0 +1,25 @@
+// Figura 6.25: fig06_25.cpp
+// Desfiguração de nomes.
+
+int square( int x ){
+
+    return x * x;
+}
+
+double square( double y ){
+
+    return y * y;
+}
+
+void nothing1( int a, float b, char c, int &d){
+
+}
+
+int nothing2( char a, int b, float &c, double &d){
+
+    return 0;
+}
+
+int main(){
+
+}
