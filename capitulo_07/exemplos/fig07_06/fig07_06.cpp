42ba0223b62aab2ee004a2ff20a52be16e4ff7e9
Anderson Misson <missonsama@gmail.com>
2022-01-07 16:59:03 -0300

    fig07_07 - Entendendo constantes const

---
+++ capitulo_07/exemplos/fig07_06/fig07_06.cpp
@@ -0,0 +1,12 @@
+// Figura 7.6: fig07_06.cpp
+// Utilizando uma variável constante adequadamente inicializada.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    const int x = 7;
+
+    cout << "O valor da constante de x é: " << x << endl;
+}
